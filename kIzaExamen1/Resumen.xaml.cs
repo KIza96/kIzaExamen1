e53f925d0aa908652c3c70e782c982ae7343c485
master <master@SMLAB408PC02_23>
2023-01-10 19:08:43 -0500

    Agregar archivos de proyecto.

---
+++ kIzaExamen1/Resumen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kIzaExamen1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string nombre, string total)
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
